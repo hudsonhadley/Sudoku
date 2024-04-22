2d085a5a9fd3ce1ed8059583ec952802c14265b0
Hudson Hadley <hudsonhadley@yahoo.com>
2024-04-22 08:40:49 -0400

    Stubs and javadoc for Coordinate

--- src/Coordinate.java
+++ src/Coordinate.java
@@ -1,3 +1,49 @@
+/**
+ * A basic coordinate class for easing the access of elements of a 2D List.
+ * @author Hudson Hadley
+ * @author Nathan Moore
+ */
 public class Coordinate {
+    /**
+     * The row of the coordinate in the 2D List.
+     */
+    private int row;
+    /**
+     * The column of the coordinate in the 2D List.
+     */
+    private int col;
 
+    /**
+     * Constructor to initialize a coordinate at (row, col).
+     * @param row the row of the coordinate we want to initialize
+     * @param col the column of the coordinate we want to initialize
+     */
+    public Coordinate(int row, int col) {
+        // TODO: Complete method
+    }
+
+    /**
+     * @return the row of the coordinate
+     */
+    public int getRow() {
+        // TODO: Complete method
+        return -1;
+    }
+
+    /**
+     * @return the column of the coordinate
+     */
+    public int getCol() {
+        // TODO: Complete method
+        return -1;
+    }
+
+    /**
+     * @return a String representation of the coordinate as (row, col)
+     */
+    @Override
+    public String toString() {
+        // TODO: Complete method
+        return "";
+    }
 }
