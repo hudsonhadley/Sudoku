2c609de18e35e214403cfe8278ccf2aa3a3b5808
Hudson Hadley <hudsonhadley@yahoo.com>
2024-04-22 15:36:56 -0400

    Stubs and javadoc

--- src/CommandLineUI.java
+++ src/CommandLineUI.java
@@ -1,2 +1,9 @@
+/**
+ * A rudimentary command line user interface. The command line offers the user a puzzle and allows them to guess. If
+ * their guess is incorrect, they are told they were wrong.
+ */
 public class CommandLineUI {
+    public static void main(String[] args) {
+        // TODO: Complete method
+    }
 }
