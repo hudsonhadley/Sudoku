dff67718f2de3b635f75b087a971dc1e0e63294a
Hudson Hadley <hudsonhadley@yahoo.com>
2024-05-02 10:50:55 -0400

    Clock added to the command line game so the user can see how long they took

--- src/CommandLineUI.java
+++ src/CommandLineUI.java
@@ -7,6 +7,48 @@ import java.util.Scanner;
  * @author Hudson Hadley
  */
 public class CommandLineUI {
+    /**
+     * Converts milliseconds into a printable String with clock time hh:mm:ss (note that hh can be more than 24)
+     * @param milliseconds the amount of milliseconds we want to convert
+     * @return the time in hh:mm:ss format
+     * @throws IllegalArgumentException if a negative number is entered
+     */
+    public static String millisToClock(long milliseconds) throws IllegalArgumentException {
+        if (milliseconds < 0)
+            throw new IllegalArgumentException("Time must be non-negative");
+
+        int seconds = (int) (milliseconds / 1000); // Divide to get seconds and cut it off at the decimal
+
+        int minutes = seconds / 60;
+        seconds %= 60;
+
+        int hours = minutes / 60;
+        minutes %= 60;
+
+        // Make sure we fill in 0's before hours numbers if they need it
+        StringBuilder hourBuilder = new StringBuilder();
+        hourBuilder.append(hours);
+
+        while (hourBuilder.length() < 2)
+            hourBuilder.insert(0, "0");
+
+        // Fill in minutes
+        StringBuilder minuteBuilder = new StringBuilder();
+        minuteBuilder.append(minutes);
+
+        while (minuteBuilder.length() < 2)
+            minuteBuilder.insert(0, "0");
+
+        // Fill in seconds
+        StringBuilder secondBuilder = new StringBuilder();
+        secondBuilder.append(seconds);
+
+        while (secondBuilder.length() < 2)
+            secondBuilder.insert(0, "0");
+
+        // Format the string properly
+        return String.format("%s:%s:%s", hourBuilder, minuteBuilder, secondBuilder);
+    }
     public static void main(String[] args) {
         // TODO: Complete method
         boolean game = true;
@@ -16,6 +58,8 @@ public class CommandLineUI {
         System.out.println(board.solveToString());
 
         Scanner scan = new Scanner(System.in);
+
+        long start = System.currentTimeMillis();
 
         while (game) {
             System.out.print("Row Guess: ");
@@ -47,6 +91,8 @@ public class CommandLineUI {
 
         }
 
+        long end = System.currentTimeMillis();
 
+        System.out.printf("Time: %s", millisToClock(end - start));
     }
 }
